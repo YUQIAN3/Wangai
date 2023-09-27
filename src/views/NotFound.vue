1e5197cd4d2f75159858c5ad083ac365aece18a4
YUQIAN3 <584882416@qq.com>
2023-09-27 11:43:42 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+<h2>当前页面不存在 404，请检查路径是否正确</h2>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+};
+
+</script>
+
+<style scoped>
+
+</style>
